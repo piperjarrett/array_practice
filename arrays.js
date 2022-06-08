fadc382a30498ad4cddc660578159c62a7c5ea7a
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-06-08 10:07:43 -0600

    Add different array method to 3 arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 var friends = ["Kailee", "Bryce", "Teegs", "Macy"];
 var age = [22, 24, 21, 21];
 var roomate = [true, true, false, false];
+
+friends.pop(); // deletes last element in friends array; Macy
+age.push(24,25) // adds 24 and 25 to to the end of the age array
+roomate.shift() // Deletes the first element in the roomate array; true
