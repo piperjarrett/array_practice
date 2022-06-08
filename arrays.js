8a4de917676c8c775b945368b3ea4e2ea097c24f
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-06-08 10:11:40 -0600

    Index positions explanantion

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ var roomate = [true, true, false, false];
 friends.pop(); // deletes last element in friends array; Macy
 age.push(24,25) // adds 24 and 25 to to the end of the age array
 roomate.shift() // Deletes the first element in the roomate array; true
+
+
+console.log(friends[0]); // I chose the 0 index place which will display the first element in the friends array; Kailee
+console.log(age[2]); // I chose the 2 index place for age which will display the third element in the age array; 21
